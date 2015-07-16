d5611d1c127fd36cdf9c2ed01a8a9ba008e1b072
Adrien Thiery <adrien.thiery@gmail.com>
2015-07-16 22:40:33 +0200

    Still figuring out what main.js is useful for

--- js/main.js
+++
@@ -1,6 +0,0 @@
-React = require(['react.min', 'tools'], function(React, tools){
-  React.render(
-    React.createElement(HLC, null),
-    document.getElementById('content')
-  );
-});
---
+++ main.js
@@ -0,0 +1,3 @@
+require(['bower_components/react/react', 'tools'], function(React, tools){
+  // Do stuff
+});
