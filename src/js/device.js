abd2a598cce95b9f757e447b5e7f693f5bff384d
Adrien Thiery <adrien.thiery@gmail.com>
2015-09-08 13:29:13 +0200

    Updated device React component for chickadee

--- src/js/device.js
+++ src/js/device.js
@@ -7,61 +7,22 @@ var Device = React.createClass({displayName: "Device",
     },
     render: function(){
       var props = this.props.props;
-      var identifier;
-      if( props.identifier ){
-        if( props.identifier instanceof Array ){
-            identifier = [ (React.createElement("div", {key: "header"}, "Identifier")) ]
-          identifier = identifier.concat( props.identifier.map( function(result, index){
-              radioDecodings = result.radioDecodings.map( function(decoding, decIndex){
-                return (React.createElement("li", null, 
-                  React.createElement("a", {href: decoding.href, target: "_blank"}, decoding.rssi), 
-                  React.createElement("p", null, "identifier: ", React.createElement("pre", null, decoding.identifier))
-                ));
-              });
-              return ( React.createElement("div", {key: index}, 
-                React.createElement("h1", null, result.value, "(type: ", result.type, ")"), 
-                React.createElement("div", null, 
-                  React.createElement("h2", null, "advHeader"), 
-                  React.createElement("pre", null, result.advHeader)
-                ), 
-                React.createElement("div", null, 
-                  React.createElement("h2", null, "advData"), 
-                  React.createElement("pre", null, result.advData)
-                ), 
-                React.createElement("h1", null, "Timestamp: ", result.timestamp), 
-                React.createElement("h1", null, "Radio Decodings"), 
-                React.createElement("ul", null, 
-                  radioDecodings
-                )
-              ) )
-           }) );
-        } else {
-          var radioDecodings = result.radioDecodings.map( function(decoding, decIndex){
-            return (React.createElement("li", null, 
-              React.createElement("a", {href: decoding.href, target: "_blank"}, decoding.rssi), 
-              React.createElement("p", null, "identifier: ", React.createElement("pre", null, decoding.identifier))
-            ));
-           });
-          var identifier = ( React.createElement("div", {key: index}, 
-             React.createElement("h1", null, result.value, "(type: ", result.type, ")"), 
-             React.createElement("div", null, 
-               React.createElement("h2", null, "advHeader"), 
-               React.createElement("pre", null, result.advHeader)
-             ), 
-             React.createElement("div", null, 
-               React.createElement("h2", null, "advData"), 
-               React.createElement("pre", null, result.advData)
-             ), 
-             React.createElement("h1", null, "Timestamp: ", result.timestamp), 
-             React.createElement("h1", null, "Radio Decodings"), 
-             React.createElement("ul", null, 
-               radioDecodings
-             )
-           ) )
-        }
+      var manufacterer;
+      if( props.manufacterer ){
+        manufacterer = ( React.createElement("p", null, "Manufacterer: ", props.manufacterer, " ") );
+      }
+      var model;
+      if( props.model ){
+        model = ( React.createElement("p", null, "Model: ", props.model) );
+      }
+      var portraitImageUrl;
+      if( props.portraitImageUrl ){
+        portraitImageUrl = ( React.createElement("img", {class: "image", src: "{props.model}"}) );
       }
       return (React.createElement("div", {title: "Device", className: "Device entity"}, 
-         identifier 
+         portraitImageUrl, 
+         manufacterer, 
+         model 
      ));
     }
   });
--- src/jsx/device.js
+++ src/jsx/device.js
@@ -7,61 +7,22 @@ var Device = React.createClass({
     },
     render: function(){
       var props = this.props.props;
-      var identifier;
-      if( props.identifier ){
-        if( props.identifier instanceof Array ){
-            identifier = [ (<div key='header'>Identifier</div>) ]
-          identifier = identifier.concat( props.identifier.map( function(result, index){
-              radioDecodings = result.radioDecodings.map( function(decoding, decIndex){
-                return (<li>
-                  <a href={decoding.href} target='_blank'>{decoding.rssi}</a>
-                  <p>identifier: <pre>{decoding.identifier}</pre></p>
-                </li>);
-              });
-              return ( <div key={index}>
-                <h1>{result.value}(type: {result.type})</h1>
-                <div>
-                  <h2>advHeader</h2>
-                  <pre>{result.advHeader}</pre>
-                </div>
-                <div>
-                  <h2>advData</h2>
-                  <pre>{result.advData}</pre>
-                </div>
-                <h1>Timestamp: {result.timestamp}</h1>
-                <h1>Radio Decodings</h1>
-                <ul>
-                  {radioDecodings}
-                </ul>
-              </div> )
-           }) );
-        } else {
-          var radioDecodings = result.radioDecodings.map( function(decoding, decIndex){
-            return (<li>
-              <a href={decoding.href} target='_blank'>{decoding.rssi}</a>
-              <p>identifier: <pre>{decoding.identifier}</pre></p>
-            </li>);
-           });
-          var identifier = ( <div key={index}>
-             <h1>{result.value}(type: {result.type})</h1>
-             <div>
-               <h2>advHeader</h2>
-               <pre>{result.advHeader}</pre>
-             </div>
-             <div>
-               <h2>advData</h2>
-               <pre>{result.advData}</pre>
-             </div>
-             <h1>Timestamp: {result.timestamp}</h1>
-             <h1>Radio Decodings</h1>
-             <ul>
-               {radioDecodings}
-             </ul>
-           </div> )
-        }
+      var manufacterer;
+      if( props.manufacterer ){
+        manufacterer = ( <p>Manufacterer: {props.manufacterer} </p> );
+      }
+      var model;
+      if( props.model ){
+        model = ( <p>Model: {props.model}</p> );
+      }
+      var portraitImageUrl;
+      if( props.portraitImageUrl ){
+        portraitImageUrl = ( <img class='image' src='{props.model}'/> );
       }
       return (<div title='Device' className='Device entity'>
-        { identifier }
+        { portraitImageUrl }
+        { manufacterer }
+        { model }
      </div>);
     }
   });
