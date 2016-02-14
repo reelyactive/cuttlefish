31eb1432080b41f9e7e5e9e27f775fca45a71a87
Adrien Thiery <adrien.thiery@gmail.com>
2016-02-14 13:41:58 -0500

    Cleaning old cormorant files

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
-window.cormorant = {
-  // Get data as argument as an object
-  // This file should know automatically which react component to send back
-  'getComponent': function(myJson){
-    if( myJson['@type'] == 'Person' )
-      return Person;
-    if( myJson['@type'] == 'Device' )
-      return Device;
-    else if( myJson['@type'] == 'Product' )
-      return Product;
-  }
+window.cuttlefish = {
+    // Get data as argument as an object
+    // This file should know automatically which react component to send back
+    getComponent: function (myJson) {
+        if( myJson['@type'] == 'Person' )
+            return Person;
+        if( myJson['@type'] == 'Device' )
+            return Device;
+        else if( myJson['@type'] == 'Product' )
+            return Product;
+    }
 };
