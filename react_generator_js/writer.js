a4b356cd4762ddb0a70ff723aa87d745f575fa11
Adrien Thiery <adrien.thiery@gmail.com>
2016-06-03 20:43:20 -0400

    Refactoring

--- react_generator_js/writer.js
+++ react_generator_js/writer.js
@@ -1,8 +1,5 @@
 var fs = require('fs');
-var components = require('./parser');
 var _ = require('lodash');
-
-var snippetAsArray = fs.readFileSync('./ES6ClassSnippet.js').toString().split('\n');
 
 function isComponent(type) {
   return type[0] === type[0].toUpperCase();
@@ -27,7 +24,7 @@ function getTemplateWord(line) {
   }
 }
 
-function getRealType(type) {
+function getReactType(type) {
   if (type.indexOf('arrayOf(') === 0) {
     var subType = type.substr(
       type.indexOf('arrayOf(') + 8,
@@ -39,68 +36,88 @@ function getRealType(type) {
   switch(type) {
     case 'url':
     case 'image':
-      return 'string';
+      return 'React.PropTypes.string';
     case 'array':
-      return 'arrayOf(React.PropTypes.' + subType + ')';
+      return 'React.PropTypes.arrayOf(React.PropTypes.' + subType + ')';
+    case 'boolean':
+      return 'React.PropTypes.bool';
     default:
-      return type;
+      return 'React.PropTypes.' + type;
   }
 }
 
+// *value* is a bit weird. It can be either *this.props.something* or *item*
+function getRepresentationOf(component, value, property, type, subType) {
+  var representation = '(';
 
-function writeRenderPreparation(file, property, type, component, isItem) {
+  switch(type) {
+    case 'component':
+      representation += "<" + subType + " {..." + value + "} />";
+      break;
+    case 'url':
+      representation += 
+        "<a className='"
+        + component.className + "_" + capitalize(property)
+        + "' href='{" + value + "}' "
+        + "target='_blank'>"
+        + capitalize(property) 
+        + "</a>";
+      break;
+    case 'image':
+      representation +=
+        "<img className='" 
+        + component.className + "_" + capitalize(property) + "' "
+        + "src='{" + value + "}' "
+        + "alt='" + capitalize(property) + "' "
+        + "/>";
+      break;
+    case 'string':
+    case 'number':
+      representation += 
+        "<p className='" 
+        + component.className + "_" + capitalize(property) + "'"
+        + ">"
+        + capitalize(property) + ": {" + value + "}" 
+        + "</p>";
+      break;
+    case 'array':
+      representation += value + '.map(function(item) {\n';
+      representation += '      return ' + getRepresentationOf(component, 'item', property, subType) + '\n';
+      representation += '    }';
+      break;
+  }
+
+  representation += ');';
+
+  return representation;
+}
+
+function writeRenderPreparation(file, component, property, type) {
   if (type.indexOf('arrayOf(') === 0) {
     var subType = type.substr(
       type.indexOf('arrayOf(') + 8,
       type.length - 8 - 1 // 8: length of 'arrayOf(', 1: length of the ')' at the end
     );
     type = 'array';
-  } else if (isComponent(type)) { // = It is another component
+  } else if (isComponent(type)) {
     subType = type;
     type = 'component';
   }
 
-  var line, value;
+  var value = 'this.props.' + property;
+  var line = '    var ' + property + ' = ' + getRepresentationOf(
+    component,
+    value,
+    property,
+    type,
+    subType
+  );
 
-  if (isItem) {
-    line = '      return (';
-    value = 'item';
-  } else {
-    line = '    var ' + property + ' = (';
-    value = 'this.props.' + property;
-  }
-
-  switch(type) {
-    case 'component':
-      line += "<" + subType + " {..." + value + "} />";
-      break;
-    case 'url':
-      line += "<a className='" + component.className + "_" + capitalize(property) + "' href='{" + value + "}' target='_blank'>" + capitalize(property) + "</a>";
-      break;
-    case 'image':
-      line += "<img className='" + component.className + "_" + capitalize(property) + "' src='{" + value + "}' alt='" + capitalize(property) +"' />";
-      break;
-    case 'string':
-    case 'number':
-      line += "<p className='" + component.className + "_" + capitalize(property) + "'>" + capitalize(property) + ": {" + value + "}</p>";
-      break;
-    case 'array':
-      line += value + '.map(function(item) {\n';
-      line += writeRenderPreparation(file, property, subType, component, true) + '\n';
-      line += '    }';
-      break;
-  }
-  line += ');';
-
-  if (isItem) {
-    return line;
-  } else {
-    write(file, line);
-    write(file, '');
-  }
+  write(file, line);
+  write(file, '');
 }
 
-function writeLine(file, templateLine, component) {
+function writeForTemplateLine(file, templateLine, component) {
   switch (getTemplateWord(templateLine)) {
     case null: // No template word in line
       write(file, templateLine);
@@ -126,7 +143,7 @@ function writeLine(file, templateLine, component) {
         if (!isComponent(type)) {
           write(
             file,
-            "    " + property + ": React.PropTypes." + getRealType(type) + ","
+            "    " + property + ": " + getReactType(type) + ","
           );
         } else {
           // Let's not put a propType on components
@@ -136,7 +153,7 @@ function writeLine(file, templateLine, component) {
     case 'render':
       write(file, "  render() {");
       _.forEach(component.properties, function(type, property) {
-        writeRenderPreparation(file, property, type, component);
+        writeRenderPreparation(file, component, property, type);
       });
       write(file, "    return (");
       write(file, "      <div class='" + component.className + "'>");
@@ -152,6 +169,8 @@ function writeLine(file, templateLine, component) {
   }
 }
 
+var snippetAsArray = fs.readFileSync('./ES6ClassSnippet.js').toString().split('\n');
+
 function writeComponent(component) {
   var file = fs.createWriteStream(
     '../src/es6/' + component.className + '.js'
@@ -160,13 +179,15 @@ function writeComponent(component) {
   console.log('--------------- file start [' + component.className + ']');
   
   snippetAsArray.forEach(function (line) {
-    writeLine(file, line, component);
+    writeForTemplateLine(file, line, component);
   });
 
   console.log('--------------- file end [' + component.className + ']');
 
   file.end();
 }
+
+var components = require('./parser');
 
 _.forEach(components, function(component, className) {
   writeComponent(component);
