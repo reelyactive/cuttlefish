f71a6fcac2fdc7c903b20fae3770d6a8878fc87a
jeffyactive <jeff@reelyactive.com>
2016-07-30 13:26:10 -0400

    Added default images

--- js/cuttlefish.js
+++ js/cuttlefish.js
@@ -6,6 +6,13 @@
 
 DEFAULT_BUBBLE_SIZE = '240px';
 BUBBLE_TEMPLATE_URL = 'bubble.html';
+TYPE_PERSON = 'Person';
+TYPE_PRODUCT = 'Product';
+TYPE_PLACE = 'Place';
+DEFAULT_IMAGE = {};
+DEFAULT_IMAGE[TYPE_PERSON] = 'images/default-person.png';
+DEFAULT_IMAGE[TYPE_PRODUCT] = 'images/default-product.png';
+DEFAULT_IMAGE[TYPE_PLACE] = 'images/default-place.png';
 
 
 angular.module('reelyactive.cuttlefish', [])
@@ -23,21 +30,28 @@ angular.module('reelyactive.cuttlefish', [])
         for(var cItem = 0; cItem < graph.length; cItem++) {
           switch(graph[cItem]["@type"]) {
             case 'schema:Person':
-              scope.person = graph[cItem];
-              scope.types.push('Person');
+              scope.person = formatItem(graph[cItem], TYPE_PERSON);
+              scope.types.push(TYPE_PERSON);
               break;
             case 'schema:Product':
-              scope.product = graph[cItem];
-              scope.types.push('Product');
+              scope.product = formatItem(graph[cItem], TYPE_PRODUCT);
+              scope.types.push(TYPE_PRODUCT);
               break;
             case 'schema:Place':
-              scope.place = graph[cItem];
-              scope.types.push('Place');
+              scope.place = formatItem(graph[cItem], TYPE_PLACE);
+              scope.types.push(TYPE_PLACE);
               break;
           }
           scope.itemID = graph[cItem]["@id"];
         }
         scope.current = scope.types[0];
+      }
+
+      function formatItem(item, type) {
+        if(!item.hasOwnProperty("schema:image")) {
+          item["schema:image"] = DEFAULT_IMAGE[type];
+        }
+        return item;
       }
 
       scope.$watch(attrs.json, function(json) {
