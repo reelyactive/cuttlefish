489d1aad1de27a862f53ab95ec0616a3c2cb3cd5
jeffyactive <jeff@reelyactive.com>
2016-09-23 23:15:06 -0400

    Standardised with bubblescape

--- js/bubble.js
+++ js/bubble.js
@@ -2,36 +2,58 @@ var Bubble = function(scope) {
   var self = this;
   self.scope = scope;
   Loader.whenAvailable('jQuery', function() {
-    Compiler.initialize();
     self.initialize();
   });
 }
 
-var BubbleIDs = {
-  duplicates: {},
-  list: []
-}
-
 Bubble.generateID = function(jsonID) {
-  if (BubbleIDs.list.indexOf(jsonID) >= 0) { // duplicate ID
-    if (!BubbleIDs.duplicates.hasOwnProperty(jsonID)) {
-      BubbleIDs.duplicates[jsonID] = {count: 1};
+  if (Bubbles.ids.indexOf(jsonID) >= 0) { // duplicate ID
+    if (!Bubbles.dupeIDs.hasOwnProperty(jsonID)) {
+      Bubbles.dupeIDs[jsonID] = {count: 1};
     }
-    BubbleIDs.duplicates[jsonID].count += 1;
-    return jsonID + BubbleIDs.duplicates[jsonID].count;
+    Bubbles.dupeIDs[jsonID].count += 1;
+    return jsonID + Bubbles.dupeIDs[jsonID].count;
   } else {
-    BubbleIDs.list.push(jsonID);
+    Bubbles.ids.push(jsonID);
     return jsonID;
   }
+}
+
+Bubble.visibleTypes = function(visible, all) {
+  
+  if (visible.length == 0) return all;
+  
+  var arr1 = visible.split(',');
+  var arr2 = all;
+  
+  var commonValues = [];
+  var i, j;
+  var arr1Length = arr1.length;
+  var arr2Length = arr2.length;
+
+  for (i = 0; i < arr1Length; i++) {
+      for (j = 0; j < arr2Length; j++) {
+          if (arr1[i] === arr2[j]) {
+              commonValues.push(arr1[i]);
+          }
+      }
+  }
+  
+  return commonValues;
+  
 }
 
 Bubble.prototype = {
   
   initialize: function(scope) {
     var self = this;
+    if (self.scope.unsupported) return false;
     self.style();
-    self.addIcons();
-    self.setHoverEvent();
+    if (self.scope.mode != 'ambient') {
+      self.addIcons();
+      self.setHoverEvent();
+    }
+    if (self.scope.motion) self.startFloating();
   },
   
   setClasses: function() {
@@ -40,15 +62,57 @@ Bubble.prototype = {
     self.bubbleClass = self.containerClass+'--photo';
     self.labelClass = self.containerClass+'--label';
     self.iconClass = self.containerClass+'--icon';
-    self.toggleClass = self.containerClass+'--toggle';
+    self.flyoutClass = self.containerClass+'--flyout';
   },
   
   setDivs: function() {
     var self = this;
     self.container = $('#'+self.scope.itemID);
-    self.bubble = $(self.bubbleClass, self.container);
-    self.label = $(self.labelClass, self.container);
-    self.toggle = $(self.toggleClass, self.container);
+    self.bubbles = $(self.bubbleClass, self.container);
+    self.bubble = self.activeBubble();
+    self.labels = $(self.labelClass, self.container);
+    self.label = $(self.labelClass, self.bubble);
+    
+    $.each(self.scope.types, function(index, type) {
+      if (type != self.scope.current) {
+        var subBubble = self.selectByType(type);
+        subBubble.addClass(self.flyoutClass.substring(1));
+      }
+    });
+    
+    self.subBubbles = $(self.flyoutClass, self.container);
+  },
+  
+  place: function() {
+    var self = this;
+    var placed = false;
+    var buffer = 60;
+    var numTries = 0;
+    var maxTries = 3000;
+    while (!placed && numTries < maxTries) {
+      var randLeft = Math.random() * ($(window).width() - self.size-buffer);
+      var randTop = Math.random() * ($(window).height() - self.size-buffer);
+      var randBottom = randTop + self.size+buffer;
+      var randRight = randLeft + self.size+buffer;
+      var collision = false;
+      $('.bubble').each(function() {
+        var left = $(this).offset().left;
+        var top = $(this).offset().top;
+        var bottom = top + self.size+buffer;
+        var right = left + self.size+buffer;
+        var overlap = !(right < randLeft || 
+                        left > randRight || 
+                        bottom < randTop || 
+                        top > randBottom);
+        if (overlap) collision = true;
+      });
+      if (!collision) {
+        //console.log('place found!');
+        placed = true;
+        self.position = {x: randLeft, y: randTop};
+      }
+      numTries++;
+    }
   },
   
   style: function() {
@@ -60,19 +124,41 @@ Bubble.prototype = {
     self.size = parseInt(self.scope.size);
     self.borderSize = self.size / 10;
     self.labelTop = self.size * 0.9;
+    self.containerSize = self.size + self.borderSize*6;
     
-    self.bubble.css({
+    //self.place();
+  
+    var emptyBox = Placement.findAvailable(self.containerSize);
+    //console.log(emptyBox);
+    self.box = emptyBox.box;
+    self.position = emptyBox.pos;
+    
+    if (!self.position) return false;
+    
+    self.container.css({
+      width: self.containerSize,
+      height: self.containerSize,
+      left: self.position.x,
+      top: self.position.y,
+      display: 'inline-block'
+    });
+    
+    self.bubbles.css({
       width: self.size,
       height: self.size,
       borderRadius: self.size,
-      borderWidth: self.borderSize
+      borderWidth: self.borderSize,
+      top: self.borderSize*2
     });
     
-    self.label.css({
-      fontSize: self.borderSize,
+    self.labels.css({
+      fontSize: self.borderSize+'px',
+      lineHeight: self.borderSize+'px',
       top: self.labelTop,
       borderRadius: self.borderSize/2
     });
+    
+    self.bubble.show();
   },
   
   parseServices: function() {
@@ -134,7 +220,7 @@ Bubble.prototype = {
               }, 200);
             });
             // add to bubble
-            console.log('Adding icon to ' + type);
+            //console.log('Adding icon to ' + type);
             icon.appendTo(self.selectByType(type));
           }
         });
@@ -197,14 +283,12 @@ Bubble.prototype = {
 
     self.hoverAnimation = {
       borderWidth: self.newBorder+'px',
-      top: '-='+self.borderIncrease+'px',
-      left: '-='+self.borderIncrease+'px'
+      top: '-='+self.borderSize*2+'px'
     };
     
     self.cssReset = {
       borderWidth: self.borderSize+'px',
-      top: 0,
-      left: 0
+      top: self.borderSize*2,
     }
   },
   
@@ -219,8 +303,12 @@ Bubble.prototype = {
       
       if (self.bubble.hasClass('hover')) return false;
       
+      Bubbles.active = true;
+      self.stopFloating();
+      self.animateFlyouts();
+      self.allOtherBubbles().fadeTo(150, 0.2);
+      
       self.bubble.addClass('hover');
-      self.toggle.css({opacity: 0});
       self.label.css({backgroundColor: 'transparent'});
       self.label.animate({
         top: self.size + (self.borderSize/3) + 'px'
@@ -247,11 +335,75 @@ Bubble.prototype = {
         backgroundColor: 'black',
         top: self.labelTop
       });
-      self.toggle.css({opacity: 1});
+      
+      self.allOtherBubbles().finish();
+      self.allOtherBubbles().css({opacity: 1.0});
+      self.resumeFloating();
+      Bubbles.active = false;
+      
+      self.subBubbles.finish();
+      self.subBubbles.hide();
       
       //Connections.redraw();
       
     });
+  },
+  
+  animateFlyouts: function() {
+    var self = this;
+    
+    var bubblePos = self.bubble.offset();
+    var subLeftStart = (self.container.width() - self.bubble.width()) / 2;
+    var subLeftEnd = bubblePos.left - (self.size / 2);
+    var subTopDirection = '+=';
+    if (bubblePos.top > $(window).height()/2) {
+      subTopDirection = '-=';
+    }
+    var subTopShift = self.size + self.borderSize*3;
+    var subLeftIncrement = self.size + self.borderSize*2;
+    if (self.subBubbles.length > 2) subLeftStart -= subLeftIncrement/2;
+    
+    $.each(self.subBubbles, function(index, subBubble) {
+      subBubble = $(subBubble).detach().appendTo('body');
+      subBubble.show();
+      subBubble.css({
+        position: 'absolute',
+        zIndex: -1,
+        top: bubblePos.top+self.borderSize,
+        left: bubblePos.left+self.borderSize,
+        borderWidth: 0,
+        opacity: 0.2
+      });
+      subBubble.animate({
+        top: subTopDirection+subTopShift+'px',
+        left: subLeftEnd,
+        opacity: 0.9
+      }, 300, function(){
+        $(this).css({zIndex: 100});
+      });
+      subLeftEnd += subLeftIncrement;
+    });
+  },
+  
+  startFloating: function() {
+    var self = this;
+    var motionClasses = ['forward-motion', 'backward-motion'];
+    var motionClass = motionClasses[Math.floor(Math.random()*motionClasses.length)];
+    var delay = Math.random()*10;
+    self.container.css({animationDelay: '-'+delay+'s'});
+    self.container.addClass(motionClass);
+  },
+  
+  stopFloating: function() {
+    var self = this;
+    self.container.css('-webkit-animation-play-state', 'paused');
+    self.container.css('animation-play-state', 'paused');
+  },
+  
+  resumeFloating: function() {
+    var self = this;
+    self.container.css('-webkit-animation-play-state', 'running');
+    self.container.css('animation-play-state', 'running');
   },
   
   checkHover: function() {
@@ -273,6 +425,11 @@ Bubble.prototype = {
     return self.selectByType(self.scope.current);
   },
   
+  allOtherBubbles: function() {
+    var self = this;
+    return $(self.bubbleClass+':not('+self.flyoutClass+')').not(self.bubble);
+  },
+  
   name: function(type) {
     var self = this;
     return self.selectByType(type).data('name');
@@ -281,9 +438,16 @@ Bubble.prototype = {
   icons: function() {
     var self = this;
     return $(self.iconClass, self.activeBubble());
+  },
+  
+  removed: function() {
+    var self = this;
+    //console.log('this bubble removed');
+    if (self.position) Placement.boxNowEmpty(self.box);
   }
   
 }
+
 
 var BubbleServices = {
   
@@ -312,7 +476,117 @@ var BubbleServices = {
   
 }
 
+
+var Bubbles = {
+  
+  active: false,
+  ids: [],
+  dupeIDs: {},
+  
+  areActive: function() {
+    var self = this;
+    return self.active;
+  },
+  
+  remove: function(id) {
+    //console.log('removed ' + id);
+  }
+  
+}
+
+Array.prototype.random = function(v) {
+  var item = this[Math.floor(Math.random()*this.length)];
+  return item;
+};
+
+var Placement = {
+  
+  initialized: false,
+  buffer: 1.1, maxNoise: 15.0,
+  origSize: 0, size: 0,
+  numRows: 0, numCols: 0,
+  spareX: 0, spareY: 0,
+  emptyBoxes: [],
+  
+  initialize: function(size) {
+    var self = this;
+    if (self.initialized) return true;
+    self.origSize = size;
+    self.size = size*self.buffer;
+    self.setEmptyBoxes();
+    self.setNoise();
+    self.initialized = true;
+  },
+  
+  setNoise: function() {
+    var self = this;
+    var multiplier = (self.buffer - 1.0);
+    self.maxNoise = multiplier * self.size;
+  },
+  
+  setEmptyBoxes: function() {
+    var self = this;
+    var windowHeight = $(window).height();
+    var windowWidth = $(window).width();
+    self.numRows = Math.floor(windowHeight / self.size);
+    self.numCols = Math.floor(windowWidth / self.size);
+    for (var row = 0; row < self.numRows; row++) {
+      for (var col = 0; col < self.numCols; col++) {
+        self.emptyBoxes.push({row, col});
+      }
+    }
+    self.spareX = windowWidth - (self.size * self.numCols);
+    self.spareY = windowHeight - (self.size * self.numRows);
+  },
+  
+  findEmptyBox: function() {
+    var self = this;
+    var randomBox = self.emptyBoxes.random();
+    // remove from array
+    var index = self.emptyBoxes.indexOf(randomBox);
+    self.emptyBoxes.splice(index, 1);
+    return randomBox;
+  },
+  
+  plusOrMinus: function() {
+    return Math.random() < 0.5 ? -1 : 1;
+  },
+  
+  convertToPosition: function(box) {
+    var self = this;
+    var left = box.col * self.size;
+    var top = box.row * self.size;
+    left += self.spareX * 0.6 * ((box.col+1) / self.numCols);
+    top += self.spareY * 0.6 * ((box.row+1) / self.numRows);
+    left += self.plusOrMinus() * Math.random() * self.maxNoise;
+    top += self.plusOrMinus() * Math.random() * self.maxNoise;
+    return {x: left, y: top};
+  },
+  
+  findAvailable: function(size) {
+    var self = this;
+    self.initialize(size);
+    if (self.emptyBoxes.length == 0) { // no more room!
+      return false;
+    }
+    var box = self.findEmptyBox();
+    var pos = self.convertToPosition(box);
+    return {box, pos};
+  },
+  
+  boxNowEmpty: function(box) {
+    var self = this;
+    self.emptyBoxes.push(box);
+  }
+  
+  
+  
+}
+
+
 var Loader = {
+  
+  loaded: {},
   
   getJQuery: function() {
     var script = document.createElement('script');
@@ -330,9 +604,14 @@ var Loader = {
   },
   
   whenAvailable: function(name, callback) {
+    var self = this;
     var interval = 10; // ms
     window.setTimeout(function() {
       if (window[name]) {
+        if (!self.loaded.hasOwnProperty(name)) { // first load
+          self.afterLoad(name);
+          self.loaded[name] = true;
+        }
         callback(window[name]);
       } else {
         window.setTimeout(arguments.callee, interval);
@@ -345,18 +624,39 @@ var Loader = {
       var img = new Image();
       img.src = arguments[i];
     }
+  },
+  
+  afterLoad: function(name) {
+    //console.log('initing after ' + name);
+    switch(name) {
+      case 'jQuery':
+        (function($){
+          $.event.special.destroyed = {
+            remove: function(o) {
+              if (o.handler) {
+                o.handler()
+              }
+            }
+          }
+        })(jQuery);
+        Compiler.initialize();
+        break;
+    }
   }
   
 }
+
 
 var AngularCompile;
 
 var Compiler = { // need Angular to recompile new elements after DOM manipulation
   
+  initialized: false,
+  
   initialize: function() {
     var self = this;
     
-    if (typeof CompilerInitialized !== "undefined") return true;
+    if (self.initialized) return true;
     
     oldPrepend = $.fn.prepend;
     $.fn.prepend = function()
@@ -392,10 +692,11 @@ var Compiler = { // need Angular to recompile new elements after DOM manipulatio
       return result;
     }
     
-    CompilerInitialized = true;
+    self.initialized = true;
   }
   
 }
+
 
 Loader.getJQuery();
 Loader.getFonts();
