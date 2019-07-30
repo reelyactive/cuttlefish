1e7774c1aa32e41dffde6daecf32e36fd681d8db
jeffyactive <jeff@reelyactive.com>
2019-07-29 21:17:47 -0400

    Added basic sameAs functionality

--- js/cuttlefish.js
+++ js/cuttlefish.js
@@ -10,6 +10,7 @@ let cuttlefish = (function() {
   const IMG_CLASS = 'card-img-top';
   const BODY_CLASS = 'card-body';
   const TITLE_CLASS = 'card-title text-truncate';
+  const SAME_AS_CLASS = 'btn-group dropup';
 
   // Render the given story in the given node
   function render(story, node) {
@@ -47,6 +48,99 @@ let cuttlefish = (function() {
       title.textContent = 'Unknown';
     }
     body.appendChild(title);
+    renderSameAs(element, body);
+  }
+
+  // Render the sameAs (links)
+  function renderSameAs(element, node) {
+    let dropup = document.createElement('div');
+    dropup.setAttribute('class', SAME_AS_CLASS);
+    node.appendChild(dropup);
+
+    let button = document.createElement('button');
+    button.setAttribute('type', 'button');
+    button.setAttribute('class', 'btn btn-secondary btn-sm dropdown-toggle');
+    button.setAttribute('data-toggle', 'dropdown');
+    button.setAttribute('aria-haspopup', 'true');
+    button.setAttribute('aria-expanded', 'false');
+    dropup.appendChild(button);
+
+    let i = document.createElement('i');
+    i.setAttribute('class', 'fas fa-ellipsis-h');
+    button.appendChild(i);
+
+    let sameAsCount = 0;
+
+    if(element.hasOwnProperty("schema:sameAs")) {
+      let menu = document.createElement('div');
+      menu.setAttribute('class', 'dropdown-menu');
+      dropup.appendChild(menu);
+
+      let sameAs = element["schema:sameAs"];
+      if(typeof sameAs === 'string') {
+        sameAs = [ sameAs ];
+      }
+
+      sameAsCount = sameAs.length;
+
+      sameAs.forEach(function(url) {
+        renderSameAsMenuItem(url, menu);
+      });
+
+    }
+    else {
+      button.setAttribute('class', 'btn btn-dark btn-sm dropdown-toggle disabled');
+    }
+
+    let count = document.createTextNode('\u00a0\u00a0' + sameAsCount);
+    button.appendChild(count);
+  }
+
+  // Render the sameAs menu item (link)
+  function renderSameAsMenuItem(url, node) {
+    let a = document.createElement('a');
+    a.setAttribute('class', 'dropdown-item');
+    a.setAttribute('href', url);
+    a.setAttribute('target', '_blank');
+
+    renderLinkIcon(url, a);
+
+    let space = document.createTextNode('\u00a0\u00a0');
+    a.appendChild(space);
+
+    let i = document.createElement('i');
+    i.setAttribute('class', 'fas fa-external-link-alt');
+    a.appendChild(i);
+
+    let urlSnippet = document.createTextNode('\u00a0' + url.split('/')[2] + '/\u2026');
+    a.appendChild(urlSnippet);
+    
+    node.appendChild(a);
+  }
+
+  // Render the link icon, if known, based on the given URL
+  function renderLinkIcon(url, node) {
+    let iClass = 'fas fa-link';
+
+    if(url.includes('github.com')) {
+      iClass = 'fab fa-github';
+    }
+    else if(url.includes('twitter.com')) {
+      iClass = 'fab fa-twitter';
+    }
+    else if(url.includes('linkedin.com')) {
+      iClass = 'fab fa-linkedin';
+    }
+    else if(url.includes('facebook.com')) {
+      iClass = 'fab fa-facebook';
+    }
+    else if(url.includes('instagram.com')) {
+      iClass = 'fab fa-instagram';
+    }
+
+    let i = document.createElement('i');
+    i.setAttribute('class', iClass);
+    node.appendChild(i);
   }
 
   // Expose the following functions and variables
